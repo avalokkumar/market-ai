d15b4027c09584695c20c9d987cbcf83b22dd691
Alok <avalokkumar@gmail.com>
2023-02-12 03:18:56 +0530

    Initial commit

---
+++ services/rule/src/test/java/com/clay/marketai/rule/RuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clay.marketai.rule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/segment/src/test/java/com/clay/marketai/segment/SegmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clay.marketai.segment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
